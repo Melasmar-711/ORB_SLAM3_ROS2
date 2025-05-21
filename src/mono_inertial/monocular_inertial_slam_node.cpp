ec457e3babb63195d6e79854202a93ef97773e41
Melasmar-711 <Mahmoudelasmar2000@outlook.com>
2025-05-21 18:06:30 +0200

    aquiring the images and imu reading alone then doing some processing to sync them, we still need a better rosbag to test with

--- src/mono_inertial/monocular_inertial_slam_node.cpp
+++ src/mono_inertial/monocular_inertial_slam_node.cpp
@@ -5,29 +5,27 @@ namespace orbslam3_ros2 {
 ORBSLAM3Node::ORBSLAM3Node(const std::string &strVocFile, 
                           const std::string &strSettingsFile, 
                           const std::string &file_name) 
-    : Node("orbslam3"),
-      mImageSub(this, "/sf/AUV/rgb_camera/image_color"),
-      mImuSub(this, "/imu/data")
+    : Node("orbslam3")
 {
     // Create SLAM system
     mpSLAM = new ORB_SLAM3::System(strVocFile, strSettingsFile, 
                                   ORB_SLAM3::System::IMU_MONOCULAR, true, 0, file_name);
     mImageScale = mpSLAM->GetImageScale();
-
-    // Initialize subscribers with QoS
-    auto qos = rclcpp::QoS(rclcpp::KeepLast(50)).best_effort();
-    
-    // Synchronize image and IMU messages
-    mSync = std::make_shared<message_filters::Synchronizer<sync_pol>>(
-        sync_pol(10), mImageSub, mImuSub);
-    mSync->registerCallback(&ORBSLAM3Node::GrabImuAndImage, this);
-
     mClahe = cv::createCLAHE(3.0, cv::Size(8, 8));
-    
-    // Initialize IMU buffer
-    mImuBuffer.clear();
-    mLastImageTime = 0.0;
-    
+
+    // Initialize subscribers
+    mImageSub = this->create_subscription<sensor_msgs::msg::Image>(
+        "/sf/AUV/rgb_camera/image_color",
+        rclcpp::SensorDataQoS(),
+        std::bind(&ORBSLAM3Node::ImageCallback, this, std::placeholders::_1));
+
+    mImuSub = this->create_subscription<sensor_msgs::msg::Imu>(
+        "/imu/data",
+        rclcpp::SensorDataQoS(),
+        std::bind(&ORBSLAM3Node::ImuCallback, this, std::placeholders::_1));
+
+    // Create OpenCV window
+    cv::namedWindow("ORB-SLAM3: Tracking", cv::WINDOW_AUTOSIZE);
     RCLCPP_INFO(this->get_logger(), "ORB-SLAM3 ROS2 node initialized");
 }
 
@@ -40,28 +38,31 @@ ORBSLAM3Node::~ORBSLAM3Node()
     mpSLAM->SaveTrajectoryEuRoC("CameraTrajectory.txt");
     mpSLAM->SaveKeyFrameTrajectoryEuRoC("KeyFrameTrajectory.txt");
     
+    // Close OpenCV window
+    cv::destroyWindow("ORB-SLAM3: Tracking");
+    
     delete mpSLAM;
 }
 
-void ORBSLAM3Node::GrabImuAndImage(const sensor_msgs::msg::Image::ConstSharedPtr& img_msg, 
-                                  const sensor_msgs::msg::Imu::ConstSharedPtr& imu_msg)
+void ORBSLAM3Node::ImuCallback(const sensor_msgs::msg::Imu::ConstSharedPtr& imu_msg)
+{
+    std::lock_guard<std::mutex> lock(mMutexImu);
+    
+    double imu_time = rclcpp::Time(imu_msg->header.stamp).seconds();
+    cv::Point3f acc(imu_msg->linear_acceleration.x, 
+                   imu_msg->linear_acceleration.y, 
+                   imu_msg->linear_acceleration.z);
+    cv::Point3f gyr(imu_msg->angular_velocity.x, 
+                   imu_msg->angular_velocity.y, 
+                   imu_msg->angular_velocity.z);
+    
+    mImuBuffer.push_back(ImuData(imu_time, acc, gyr));
+    mConVarImu.notify_one();
+}
+
+void ORBSLAM3Node::ImageCallback(const sensor_msgs::msg::Image::ConstSharedPtr& img_msg)
 {
     try {
-        // Process IMU data first
-        {
-            std::lock_guard<std::mutex> lock(mMutexImu);
-            
-            double t = rclcpp::Time(img_msg->header.stamp).seconds();
-            cv::Point3f acc(imu_msg->linear_acceleration.x, 
-                           imu_msg->linear_acceleration.y, 
-                           imu_msg->linear_acceleration.z);
-            cv::Point3f gyr(imu_msg->angular_velocity.x, 
-                            imu_msg->angular_velocity.y, 
-                            imu_msg->angular_velocity.z);
-            
-            mImuBuffer.push_back(ImuData(t, acc, gyr));
-        }
-
         // Process image
         cv_bridge::CvImageConstPtr cv_ptr;
         cv_ptr = cv_bridge::toCvShare(img_msg, sensor_msgs::image_encodings::MONO8);
@@ -70,65 +71,65 @@ void ORBSLAM3Node::GrabImuAndImage(const sensor_msgs::msg::Image::ConstSharedPtr
         cv::Mat im;
         mClahe->apply(cv_ptr->image, im);
 
-        // Handle image scaling
-        if(mImageScale != 1.f)
-        {
-            int width = im.cols * mImageScale;
-            int height = im.rows * mImageScale;
-            cv::resize(im, im, cv::Size(width, height));
-        }
+        // Display the frame
+        cv::imshow("ORB-SLAM3: Tracking", im);
+        cv::waitKey(1);
 
-        // Get timestamp
+        // Process frame with IMU data
         double tframe = rclcpp::Time(img_msg->header.stamp).seconds();
-
-        // Get IMU measurements since last image
-        std::vector<ORB_SLAM3::IMU::Point> vImuMeas;
-        GetImuMeasurements(tframe, vImuMeas);
-
-        // Track the image with IMU data
-        mpSLAM->TrackMonocular(im, tframe, vImuMeas);
-
-        // Update last image time
-        mLastImageTime = tframe;
+        ProcessFrame(im, tframe);
     }
     catch (const cv_bridge::Exception& e) {
         RCLCPP_ERROR(this->get_logger(), "cv_bridge exception: %s", e.what());
-        return;
-    }
-    catch (const std::exception& e) {
-        RCLCPP_ERROR(this->get_logger(), "Exception: %s", e.what());
-        return;
     }
 }
 
-void ORBSLAM3Node::GetImuMeasurements(double tframe, std::vector<ORB_SLAM3::IMU::Point>& vImuMeas)
+void ORBSLAM3Node::ProcessFrame(const cv::Mat& im, double tframe)
 {
-    std::lock_guard<std::mutex> lock(mMutexImu);
+    std::vector<ORB_SLAM3::IMU::Point> vImuMeas;
     
-    if(mImuBuffer.empty())
-        return;
-        
-    // Remove IMU measurements older than last image
-    while(!mImuBuffer.empty() && mImuBuffer.front().t <= mLastImageTime)
-    {
-        mImuBuffer.pop_front();
+    // Wait for IMU data if this is the first frame
+    if(mFirstImage) {
+        std::unique_lock<std::mutex> lock(mMutexImu);
+        mConVarImu.wait(lock, [this]{return !mImuBuffer.empty();});
+        mFirstImage = false;
     }
 
-    // Get IMU measurements between last image and current image
-    for(const auto& imu_data : mImuBuffer)
+    // Get IMU measurements since last image
     {
-        if(imu_data.t <= tframe)
-        {
-            vImuMeas.push_back(ORB_SLAM3::IMU::Point(
-                imu_data.acc.x, imu_data.acc.y, imu_data.acc.z,
-                imu_data.gyr.x, imu_data.gyr.y, imu_data.gyr.z,
-                imu_data.t));
+        std::lock_guard<std::mutex> lock(mMutexImu);
+        
+        // Remove old IMU measurements
+        while(!mImuBuffer.empty() && mImuBuffer.front().t <= mLastImageTime) {
+            mImuBuffer.pop_front();
         }
-        else
-        {
-            break;
+
+        // Get measurements up to current frame
+        for(const auto& imu_data : mImuBuffer) {
+            if(imu_data.t <= tframe) {
+                vImuMeas.push_back(ORB_SLAM3::IMU::Point(
+                    imu_data.acc.x, imu_data.acc.y, imu_data.acc.z,
+                    imu_data.gyr.x, imu_data.gyr.y, imu_data.gyr.z,
+                    imu_data.t));
+            } else {
+                // Include one measurement beyond for interpolation
+                vImuMeas.push_back(ORB_SLAM3::IMU::Point(
+                    imu_data.acc.x, imu_data.acc.y, imu_data.acc.z,
+                    imu_data.gyr.x, imu_data.gyr.y, imu_data.gyr.z,
+                    imu_data.t));
+                break;
+            }
         }
     }
+
+    // Track only if we have IMU data
+    if(!vImuMeas.empty()) {
+        mpSLAM->TrackMonocular(im, tframe, vImuMeas);
+    } else {
+        RCLCPP_WARN(this->get_logger(), "Skipping frame - no IMU data available");
+    }
+
+    mLastImageTime = tframe;
 }
 
 } // namespace orbslam3_ros2
--- src/mono_inertial/monocular_inertial_slam_node.hpp
+++ src/mono_inertial/monocular_inertial_slam_node.hpp
@@ -1,18 +1,15 @@
 #ifndef ORBSLAM3_ROS2_ORBSLAM3_NODE_HPP
 #define ORBSLAM3_ROS2_ORBSLAM3_NODE_HPP
 
-#include <memory>
 #include <deque>
 #include <mutex>
+#include <condition_variable>
 
 #include <rclcpp/rclcpp.hpp>
-#include <message_filters/subscriber.h>
-#include <message_filters/synchronizer.h>
-#include <message_filters/sync_policies/approximate_time.h>
 #include <sensor_msgs/msg/image.hpp>
 #include <sensor_msgs/msg/imu.hpp>
 #include <cv_bridge/cv_bridge.h>
-#include <opencv2/core/core.hpp>
+#include <opencv2/opencv.hpp>
 
 #include <System.h>
 #include "ImuTypes.h"
@@ -24,15 +21,10 @@ class ORBSLAM3Node : public rclcpp::Node
 public:
     ORBSLAM3Node(const std::string &strVocFile, 
                 const std::string &strSettingsFile, 
-                const std::string &file_name);
+                const std::string &file_name = "");
     ~ORBSLAM3Node();
 
 private:
-    // Define synchronization policy
-    typedef message_filters::sync_policies::ApproximateTime<
-        sensor_msgs::msg::Image, 
-        sensor_msgs::msg::Imu> sync_pol;
-
     struct ImuData {
         double t;
         cv::Point3f acc;
@@ -42,23 +34,28 @@ private:
             : t(_t), acc(_acc), gyr(_gyr) {}
     };
 
-    void GrabImuAndImage(const sensor_msgs::msg::Image::ConstSharedPtr& img_msg, 
-                        const sensor_msgs::msg::Imu::ConstSharedPtr& imu_msg);
-    void GetImuMeasurements(double tframe, std::vector<ORB_SLAM3::IMU::Point>& vImuMeas);
+    // Callbacks
+    void ImageCallback(const sensor_msgs::msg::Image::ConstSharedPtr& img_msg);
+    void ImuCallback(const sensor_msgs::msg::Imu::ConstSharedPtr& imu_msg);
+    void ProcessFrame(const cv::Mat& im, double tframe);
 
+    // SLAM system
     ORB_SLAM3::System* mpSLAM;
     float mImageScale;
     cv::Ptr<cv::CLAHE> mClahe;
     
-    message_filters::Subscriber<sensor_msgs::msg::Image> mImageSub;
-    message_filters::Subscriber<sensor_msgs::msg::Imu> mImuSub;
-    std::shared_ptr<message_filters::Synchronizer<sync_pol>> mSync;
+    // ROS2 subscribers
+    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr mImageSub;
+    rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr mImuSub;
     
+    // Data buffers and synchronization
     std::deque<ImuData> mImuBuffer;
     std::mutex mMutexImu;
-    double mLastImageTime;
+    std::condition_variable mConVarImu;
+    double mLastImageTime = 0.0;
+    bool mFirstImage = true;
 };
 
 } // namespace orbslam3_ros2
 
-#endif // ORBSLAM3_ROS2_ORBSLAM3_NODE_HPP
+#endif
